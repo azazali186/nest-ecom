66454226a63dc0eca5de09f3e91a09327ea9733e
Jenny <jenny.maya186@gmail.com>
2023-11-25 15:40:08 +0700

    Updated

--- src/dto/login.dto.ts
+++ src/dto/login.dto.ts
@@ -1,38 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsString,
-  IsNotEmpty,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty()
-  @IsString({
-    message: 'INVALID_FORMAT_USERNAME',
-  })
-  @MaxLength(20, {
-    message: 'MAX_LENGTH_USERNAME_ERROR',
-  })
-  @Matches(/^[A-Za-z0-9]+$/, {
-    message: 'INVALID_FORMAT_USERNAME',
-  })
   @IsNotEmpty({
     message: 'USERNAME_IS REQUIRED',
   })
   username: string;
 
   @ApiProperty()
-  @IsString({
-    message: 'INVALID_FORMAT_PASSWORD',
-  })
-  @MinLength(6, {
-    message: 'MIN_LENGTH_PASSWORD_ERROR',
-  })
-  @Matches(/^(?=.*?[A-Za-z])(?=.*?[0-9]).*$/, {
-    message: 'INVALID_FORMAT_PASSWORD',
-  })
   @IsNotEmpty({
     message: 'PASSWORD_IS REQUIRED',
   })
