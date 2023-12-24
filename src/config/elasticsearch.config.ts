b198d4fa1022a1bf57558a94705abc1510c6cd04
Ali Azaz <azaz.ali186@gmail.com>
2023-12-24 11:12:18 +0700

    updated

--- src/config/elasticsearch.config.ts
+++ src/config/elasticsearch.config.ts
@@ -7,8 +7,8 @@ export const ElasticsearchConfig = {
   node: process.env.ELASTICSEARCH_URL || 'https://127.0.0.1:9200',
   auth: {
     username: 'elastic', // Replace with your Elasticsearch username (if applicable)
-    password: 'uW0jF1K9Evlwn*SKgCqW', // Replace with your office mac Elasticsearch password (if applicable)
-    // password: 'Wg=8BIsqIXnkE+z*1T5O', // Replace with your home laptop Elasticsearch password (if applicable)
+    // password: 'uW0jF1K9Evlwn*SKgCqW', // Replace with your office mac Elasticsearch password (if applicable)
+    password: 'Wg=8BIsqIXnkE+z*1T5O', // Replace with your home laptop Elasticsearch password (if applicable)
   },
   tls: {
     ca: readFileSync(certificatePath),
