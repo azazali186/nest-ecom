904c8e31635c2c1d3b716089bb1e5233408c7a7d
Jenny <jenny.maya186@gmail.com>
2024-01-03 08:29:06 +0700

    Telegram git commit bot dynamic commit message testing

--- script/gitTelegramNotifier.js
+++ script/gitTelegramNotifier.js
@@ -2,15 +2,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const { Telegram } = require('telegraf');
 
-const sendTelegramMessage = () => {
+const sendTelegramMessage = (msg) => {
   try {
     const telegram = new Telegram(
       '6452255074:AAF7i_n41WYPmVZmHMnFK-tniK0ubomCz9Y',
     );
-    telegram.sendMessage('-1002106618755', 'Git Commit Done');
+    telegram.sendMessage('-1002106618755', msg);
   } catch (e) {
     console.log('ee::::>>>>' + e);
   }
 };
 
-sendTelegramMessage();
+const commitMessageArg = process.argv[2];
+
+sendTelegramMessage(commitMessageArg);
